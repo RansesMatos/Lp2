760e5e61695d0b754abac4c21ff6856c77193d29
aefe17 <53502052+aefe17@users.noreply.github.com>
2019-10-24 20:38:58 -0400

    Create Modelo.cs

---
+++ PracticaLp2Curso/PracticaLp2Curso/Modelo.cs
@@ -0,0 +1,14 @@
+using system;
+using linq;
+using system.IOExceptions;
+
+namespace Modelo
+{
+  public class Modelo
+  {
+    public int Modelo_ID {get;set;}
+    public string Nombre {get;set;}
+    public int numChasis {get;set;}
+    public int Marca_ID {get;set;}
+  }
+}
