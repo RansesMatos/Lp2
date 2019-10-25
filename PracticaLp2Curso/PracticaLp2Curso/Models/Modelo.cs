7e9ffcbb26af3674b144d31d1e216fac7de0708d
WENMY RANSES MATOS <ranses94@gmail.com>
2019-10-24 21:09:15 -0400

    Dependecias

--- PracticaLp2Curso/PracticaLp2Curso/Models/Modelo.cs
+++ PracticaLp2Curso/PracticaLp2Curso/Models/Modelo.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace Models
 {
   public class Vehiculo
   {
-    [Primary Key]
+    [Key]
     public int Vehiculo_ID {get;set;}
     public int Modelo_ID {get;set;}
     public string Nombre {get;set;}
