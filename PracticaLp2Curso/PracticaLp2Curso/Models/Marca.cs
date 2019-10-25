a7f14deceac40ee39ba387a9fe7e441aee3cd16c
WENMY RANSES MATOS <ranses94@gmail.com>
2019-10-24 20:49:10 -0400

    marca

---
+++ PracticaLp2Curso/PracticaLp2Curso/Models/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaLp2Curso.Models
+{
+    public class Marca
+    {
+        public int ID_marca { get; set; }
+        public string Nombre_marca { get; set; }
+
+    }
+}
