9a0a0fe38b8c60a9feae2f833689ea0a3fcbfb0d
Andres Fanjul <aefe15@gmail.com>
2019-10-24 21:20:31 -0400

    Añadido String Descripcion

--- PracticaLp2Curso/PracticaLp2Curso/Models/Vehiculo.cs
+++ PracticaLp2Curso/PracticaLp2Curso/Models/Vehiculo.cs
@@ -11,6 +11,8 @@ namespace Models
     public int Vehiculo_ID {get;set;}
     public int Modelo_ID {get;set;}
     public string Nombre {get;set;}
+    [MaxLength(50)]
+    public string Descripcion{ get; set; }
     public int numChasis {get;set;}
     public int Marca_ID {get;set;}
   }
