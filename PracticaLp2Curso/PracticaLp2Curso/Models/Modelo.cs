8cb4c7d8a1a777728e6a1a726062c46e3b63ab76
wellington de la cruz <well-cruz@hotmail.com>
2019-10-24 21:08:28 -0400

    Agregando modelo

--- PracticaLp2Curso/PracticaLp2Curso/Models/Modelo.cs
+++ PracticaLp2Curso/PracticaLp2Curso/Models/Modelo.cs
@@ -1,18 +1,15 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-
-namespace Models
+namespace PracticaLp2Curso.Models
 {
-  public class Vehiculo
-  {
-    [Primary Key]
-    public int Vehiculo_ID {get;set;}
-    public int Modelo_ID {get;set;}
-    public string Nombre {get;set;}
-    public int numChasis {get;set;}
-    public int Marca_ID {get;set;}
-  }
+    public class Modelo
+    {
+        [Key]
+        public int Id_Modelo { get; set; }
+        public string Nombre { get; set; }
+    }
 }
---
+++ PracticaLp2Curso/PracticaLp2Curso/Models/Vehiculo.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+  public class Vehiculo
+  {
+    [Key]
+    public int Vehiculo_ID {get;set;}
+    public int Modelo_ID {get;set;}
+    public string Nombre {get;set;}
+    public int numChasis {get;set;}
+    public int Marca_ID {get;set;}
+  }
+}
