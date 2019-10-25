1ee1be936cfa4968d4051fda3551016719fa2ae8
aefe17 <53502052+aefe17@users.noreply.github.com>
2019-10-24 20:53:51 -0400

    Update Modelo.cs

--- PracticaLp2Curso/PracticaLp2Curso/Models/Modelo.cs
+++ PracticaLp2Curso/PracticaLp2Curso/Models/Modelo.cs
@@ -6,6 +6,8 @@ namespace Models
 {
   public class Vehiculo
   {
+    [Primary Key]
+    public int Vehiculo_ID {get;set;}
     public int Modelo_ID {get;set;}
     public string Nombre {get;set;}
     public int numChasis {get;set;}
