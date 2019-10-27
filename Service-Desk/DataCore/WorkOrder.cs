936b1ead69311531c73504b7198cfb550e2e5a26
Andres Fanjul <aefe15@gmail.com>
2019-10-27 17:54:42 -0400

    Upadated Class 1

---
+++ Service-Desk/DataCore/WorkOrder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DataCore
+{
+    public class WorkOrder
+    {
+        private int workOrderID { get; set; }
+        private int requesterID { get; set; }
+        private int createdByID { get; set; }
+        private DateTime createdTime { get; set; }
+        private DateTime respondedTime { get; set; }
+        private DateTime dueTime { get; set; }
+        private DateTime lastUpdate { get; set; }
+        private DateTime completedTime { get; set; }
+        private int TimeSpentOn { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private int modeID { get; set; }
+        private int departmentID { get; set; }
+        private DateTime fr_DueTime { get; set; }
+
+    }
+}
