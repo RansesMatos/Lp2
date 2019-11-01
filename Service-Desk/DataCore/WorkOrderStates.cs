f0af6492ff61f003026a53fba79d738fc07aef53
aefe17 <53502052+aefe17@users.noreply.github.com>
2019-10-31 20:17:53 -0400

    Added WorkOrderStates

---
+++ Service-Desk/DataCore/WorkOrderStates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataCore
+{
+    public class WorkOrderStates
+    {
+        private int workOrderID { get; set; }
+        private int categoryID { get; set; }
+        private int subcategoryID { get; set; }
+        private int itemID { get; set; }
+        private int ownerID { get; set; }
+        private DateTime assignedTime { get; set; }
+        private int statusID { get; set; }
+        private int levelID { get; set; }
+        private bool isOverdue { get; set; }
+        private bool isEscalated { get; set; }
+        private bool isfrOverdue { get; set; }
+        private string resolution_Comments { get; set; }
+    }
+}
