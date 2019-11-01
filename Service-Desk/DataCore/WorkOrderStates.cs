ef22205257279ffc912d0270d74116d6ebb8fa72
aefe17 <53502052+aefe17@users.noreply.github.com>
2019-10-31 20:22:31 -0400

    Added DataAnnotations

--- Service-Desk/DataCore/WorkOrderStates.cs
+++ Service-Desk/DataCore/WorkOrderStates.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace DataCore
 {
     public class WorkOrderStates
     {
+        [Key]
         private int workOrderID { get; set; }
+        [ForeignKey]
         private int categoryID { get; set; }
+        [ForeignKey]
         private int subcategoryID { get; set; }
+        [ForeignKey]
         private int itemID { get; set; }
+        [ForeignKey]
         private int ownerID { get; set; }
         private DateTime assignedTime { get; set; }
         private int statusID { get; set; }
