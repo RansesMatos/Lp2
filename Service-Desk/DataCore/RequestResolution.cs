7ebf3b9f1cb95dd4620512886bb4ad7d4e7c95a2
aefe17 <53502052+aefe17@users.noreply.github.com>
2019-11-02 18:28:25 -0400

    Added Request Resolution and Deleted WorkOrderToDescription

---
+++ Service-Desk/DataCore/RequestResolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataCore
+{
+    public class RequestResolution
+    {
+        [Key]
+        public int requestID { get; set; }
+        [MaxLength(50)]
+        public string resolution { get; set; }
+    }
+}
