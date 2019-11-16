a448b43d0ad5602b93e82335843cd1b90aeaa9e5
WENMY RANSES MATOS <ranses94@gmail.com>
2019-11-16 19:15:04 -0300

    bien

---
+++ Service-Desk/DataCore/WorkOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataCore
+{
+    public class WorkOrder
+    {
+        [Key] 
+        private int workOrderID { get; set; }
+        [ForeignKey]
+        private int requesterID { get; set; }
+        [ForeignKey]
+        private int createdByID { get; set; }
+        private DateTime createdTime { get; set; }
+        private DateTime respondedTime { get; set; }
+        private DateTime dueTime { get; set; }
+        private DateTime lastUpdate { get; set; }
+        private DateTime completedTime { get; set; }
+        private int TimeSpentOn { get; set; }
+        [StringLength(50)]
+        private string Title { get; set; }
+        [StringLength(150)]
+        private string Description { get; set; }
+        [ForeignKey]
+        private int modeID { get; set; }
+        [ForeignKey]
+        private int departmentID { get; set; }
+        private DateTime fr_DueTime { get; set; }
+
+    }
+}
---
+++ Service-Desk/DataCore/WorkOrderStates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DataCore
+{
+    public class WorkOrderStates
+    {
+        [Key]
+        private int workOrderID { get; set; }
+        [ForeignKey]
+        private int categoryID { get; set; }
+        [ForeignKey]
+        private int subcategoryID { get; set; }
+        [ForeignKey]
+        private int itemID { get; set; }
+        [ForeignKey]
+        private int ownerID { get; set; }
+        private DateTime assignedTime { get; set; }
+        private int statusID { get; set; }
+        private int levelID { get; set; }
+        private bool isOverdue { get; set; }
+        private bool isEscalated { get; set; }
+        private bool isfrOverdue { get; set; }
+        private string resolution_Comments { get; set; }
+    }
+}
